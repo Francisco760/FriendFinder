2c3c81f43fa3438d46ebb0cd9e43cd7ffb759fe5
Francisco760 <f.arechiga@hotmail.com>
2018-07-21 23:04:26 -0600

    friend app

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friendsArray = [
+    {
+        "name": "Captain America",
+        "photo": "http://moviehole.net/img/captain-america-avengers-infinity-war.jpg",
+        "scores": ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+    },
+    {
+        "name": "Iron Man",
+        "photo": "http://astrokentico.s3.amazonaws.com/rojakdaily/media/1nichoras/iron%20man%20model%20prime/iron-man.jpg?ext=.jpg",
+        "scores": ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+    },
+    {
+        "name": "Deadpool",
+        "photo": "https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/02/08/Pictures/_33b2ca74-0cc1-11e8-ba67-a8387f729390.jpeg",
+        "scores": ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    },
+    {
+        "name": "Hulk",
+        "photo": "https://cdn.player.one/sites/player.one/files/2017/10/14/5014474-5893830059-hulk.jpg",
+        "scores": ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
+    },
+    {
+        "name": "Loki",
+        "photo": "https://vignette.wikia.nocookie.net/epicrapbattlesofhistory/images/a/a8/Loki_Based_On.png/revision/latest?cb=20160903135331",
+        "scores": ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+    },
+]
+
+module.exports = friendsArray;
